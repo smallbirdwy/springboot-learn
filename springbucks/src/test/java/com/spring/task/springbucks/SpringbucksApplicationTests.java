ebd8eda99efa907fcba47130b9fd2420c1b6bd1e
smallbird <smallbirdwy@163.com>
2022-05-31 22:27:44 +0800

    first commit

---
+++ springbucks/src/test/java/com/spring/task/springbucks/SpringbucksApplicationTests.java
@@ -0,0 +1,17 @@
+package com.spring.task.springbucks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringbucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("hehehehh");
+	}
+
+}
